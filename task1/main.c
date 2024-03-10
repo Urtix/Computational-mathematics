2558537b225193ec595e47679f36d19042960534
Urtix <es.perevalov@mail.ru>
2024-03-10 03:31:47 +0300

    feat: Implementation algorithm 11.5

--- task1/main.c
+++ task1/main.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+#include <time.h>
 
 int main()
 {
-    int threads = 8;
+    int threads = 4;
     double t1 = omp_get_wtime();
     int N = 3000;
+    double h = 1.0 / (N + 1);
+    double default_f = 0;
+    printf("Start\n");
+
     double **f = (double **)malloc((N + 2) * sizeof(double *));
     for (int i = 0; i < N + 2; i++)
         f[i] = (double *)malloc((N + 2) * sizeof(double));
-    double h = 1.0 / (N + 1);
-    double default_f = 0;
+
     for (int i = 0; i < N + 2; i++)
     {
-        f[i] = (double *)malloc((N + 2) * sizeof(double));
         for (int j = 0; j < N + 2; j++)
         {
             double x = (double)i * h;
@@ -42,11 +45,13 @@ int main()
             }
         }
     }
-    srand(69);
+
+    srand(time(NULL));
 
     double **u = (double **)malloc((N + 2) * sizeof(double *));
     for (int i = 0; i < N + 2; i++)
         u[i] = (double *)malloc((N + 2) * sizeof(double));
+
 
     for (int i = 0; i < N + 2; i++)
     {
@@ -55,6 +60,7 @@ int main()
             u[i][j] = (double)(rand() % (100 - (-100) + 1) + (-100));
         }
     }
+
     double eps = 0.1;
     double dmax; // максимальное изменение значений u
     int k = 0;
@@ -68,31 +74,53 @@ int main()
         dmax = 0.0;
         int i, j;
         double temp;
-        double dm;
+        double *dm = calloc(N, sizeof(*dm));
         double d;
-#pragma omp parallel for shared(u, N, dmax) private(i, j, temp, dm, d)
-        for (int i = 1; i < N + 1; i++)
+        //
+        for (int nx = 1; nx < N + 1; nx++)
         {
-            dm = 0;
-            for (int j = 1; j < N + 1; j++)
+            dm[nx] = 0;
+#pragma omp parallel for shared(u, nx, dm) private(i, j, temp, d)
+            for (i = 1; i < nx + 1; i++)
             {
+                j = nx - i;
                 temp = u[i][j];
                 u[i][j] = 0.25 * (u[i - 1][j] + u[i + 1][j] + u[i][j - 1] + u[i][j + 1] - h * h * f[i][j]);
                 d = fabs(temp - u[i][j]);
-                if (dm < d)
-                    dm = d;
-            }
+                if (dm[i] < d)
+                    dm[i] = d;
+            } // конец параллельной области
+        }
+        // затухание волны
+        for (int nx = N - 2; nx > -1; nx--)
+        {
+#pragma omp parallel for shared(u, nx, dm) private(i, j, temp, d)
+            for (i = N - nx + 1; i < N + 1; i++)
+            {
+                j = 2 * N - nx - i;
+                temp = u[i][j];
+                u[i][j] = 0.25 * (u[i - 1][j] + u[i + 1][j] + u[i][j - 1] + u[i][j + 1] - h * h * f[i][j]);
+                d = fabs(temp - u[i][j]);
+                if (dm[i] < d)
+                    dm[i] = d;
+            } // конец параллельной области
+        }
+#pragma omp parallel for shared(N, dm, dmax) private(i)
+        for (i = 1; i < N + 1; i++)
+        {
             omp_set_lock(&dmax_lock);
-            if (dmax < dm)
-                dmax = dm;
+            if (dmax < dm[i])
+                dmax = dm[i];
             omp_unset_lock(&dmax_lock);
         }
     } while (dmax > eps);
     double t2 = omp_get_wtime();
     double time = t2 - t1;
+
     printf("N: %d\n", N);
     printf("thread: %d\n", threads);
     printf("time: %f sec \n", time);
     printf("iteration: %d\n", k);
+
     return 1;
 }
