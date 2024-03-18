bf3125398c2e97ef8653120f26a33b3ad4292441
Urtix <es.perevalov@mail.ru>
2024-03-18 20:55:11 +0300

    feat: Сhanging the README and adding results

--- task1/main.c
+++ task1/main.c
@@ -20,6 +20,139 @@ double **allocate2DArray(int N)
     return array;
 }
 
+double u1(int i, int j, int N, double x, double y)
+{
+    if ((i == N + 1) || (i == 0) || (j == 0) || (j == N + 1))
+    {
+        // 1000·x^3·y - 2000·y^4+500·y^3+x^2·y^3-700·x+250·y
+        return 1000.0 * pow(x, 3) * y - 2000.0 * pow(y, 4) + 500.0 * pow(y, 3) + pow(x, 2) * pow(y, 3) - 700 * x + 250 * y;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+double f1(double x, double y)
+{
+    // 6000·x·y+2·y^3  +  6·x^2·y-24000·y^2+3000·y
+    return 6000 * x * y + 2 * pow(y, 3) + 6 * pow(x, 2) * y - 24000 * pow(y, 2) + 3000 * y;
+}
+
+double u2(int i, int j, int N, double x, double y)
+{
+    if ((i == N + 1) || (i == 0) || (j == 0) || (j == N + 1))
+    {
+        // 10x^3 + 20y^3
+        return 10.0 * pow(x, 3) * y + 20.0 * pow(y, 3);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+double f2(double x, double y)
+{
+    // 60x + 120y
+    return 60 * x + 120 * y;
+}
+
+double u3(int i, int j, int N, double x, double y)
+{
+    if ((i == N + 1) || (i == 0) || (j == 0) || (j == N + 1))
+    {
+        // 100x^3 + 200y^3
+        return 100.0 * pow(x, 3) + 200.0 * pow(y, 3);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+double f3(double x, double y)
+{
+    // 600x + 1200y
+    return 600 * x + 1200 * y;
+}
+
+double u4(int i, int j, int N, double x, double y)
+{
+    if ((i == N + 1) || (i == 0) || (j == 0) || (j == N + 1))
+    {
+        // 1000x^3 + 2000y^3
+        return 1000.0 * pow(x, 3) + 2000.0 * pow(y, 3);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+double f4(double x, double y)
+{
+    // 6000x + 12000y
+    return 6000 * x + 12000 * y;
+}
+
+double u5(int i, int j, int N, double x, double y)
+{
+    if ((i == N + 1) || (i == 0) || (j == 0) || (j == N + 1))
+    {
+        // 10x^4 + 20y^4
+        return 10.0 * pow(x, 4) * y + 20.0 * pow(y, 4);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+double f5(double x, double y)
+{
+    // 120x + 240y
+    return 120 * pow(x, 2) + 240 * pow(y, 2);
+}
+
+double u6(int i, int j, int N, double x, double y)
+{
+    if ((i == N + 1) || (i == 0) || (j == 0) || (j == N + 1))
+    {
+        // 10x^5 + 20y^3
+        return 10.0 * pow(x, 5) * y + 20.0 * pow(y, 5);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+double f6(double x, double y)
+{
+    // 200x^3 + 400y^3
+    return 200 * pow(x, 3) + 400 * pow(y, 3);
+}
+
+double u7(int i, int j, int N, double x, double y)
+{
+    if ((i == N + 1) || (i == 0) || (j == 0) || (j == N + 1))
+    {
+        // 10x^50 + 20y^50
+        return 10.0 * pow(x, 50) * y + 20.0 * pow(y, 50);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+double f7(double x, double y)
+{
+    // 24500x^48 + 49000y^48
+    return 24500 * pow(x, 48) + 49000 * pow(y, 48);
+}
+
 void initialize_f_u(double **u, double **f, int N, double h)
 {
     for (int i = 0; i < N + 2; i++)
@@ -28,15 +161,8 @@ void initialize_f_u(double **u, double **f, int N, double h)
         {
             double x = i * h;
             double y = j * h;
-            if ((i == N + 1) || (i == 0) || (j == 0) || (j == N + 1))
-            {
-                u[i][j] = ((1000.0 * pow(x, 3) * y) - 2000.0 * pow(y, 4) + 500.0 * pow(y, 3) + pow(x, 2) * pow(y, 3) - 700 * x + 250 * y); // 1000·x^3·y - 2000·y^4+500·y^3+x^2·y^3-700·x+250·y
-            }
-            else
-            {
-                u[i][j] = 0;
-            }
-            f[i][j] = 6000 * x * y + 2 * pow(y, 3) + 6 * pow(x, 2) * y - 24000 * pow(y, 2) + 3000 * y; // 6000·x·y+2·y^3  +  6·x^2·y-24000·y^2+3000·y
+            u[i][j] = u1(i, j, N, x, y);
+            f[i][j] = f1(x, y);
         }
     }
 }
@@ -124,40 +250,44 @@ void free_memory(double **u, double **f, double *dm, int N)
 
 int main()
 {
-
-    int grids[5] = {100, 300, 500, 1000, 3000};
-    int num_grids = sizeof(grids) / sizeof(grids[0]);
-    for (int threads = 1; threads <= 4; threads += 3)
+    for (int x = 1; x < 11; x++)
     {
-        for (int n = 0; n < num_grids; n++)
+        printf("Experiment number %d. \n", x);
+        int grids[5] = {100, 300, 500, 1000, 3000};
+        int num_grids = sizeof(grids) / sizeof(grids[0]);
+        for (int threads = 1; threads <= 4; threads += 3)
         {
-            double t1 = omp_get_wtime();
-            int N = grids[n];
-            double h = 1.0 / (N + 1);
+            for (int n = 0; n < num_grids; n++)
+            {
+                double t1 = omp_get_wtime();
+                int N = grids[n];
+                double h = 1.0 / (N + 1);
 
-            printf("Start\n");
+                printf("Start\n");
 
-            double **f = allocate2DArray(N);
-            double **u = allocate2DArray(N);
-            initialize_f_u(u, f, N, h);
+                double **f = allocate2DArray(N);
+                double **u = allocate2DArray(N);
+                initialize_f_u(u, f, N, h);
 
-            double dmax; // максимальное изменение значений u
-            int k = 0;
+                double dmax; // максимальное изменение значений u
+                int k = 0;
 
-            omp_set_num_threads(threads);
-            int num_block = (N - 2) / NB + 1;
-            double *dm = calloc(num_block, sizeof(*dm));
-            k = wave_propagation(num_block, u, f, dm, N, h);
-            double t2 = omp_get_wtime();
-            double tend_ie = t2 - t1;
+                omp_set_num_threads(threads);
+                int num_block = (N - 2) / NB + 1;
+                double *dm = calloc(num_block, sizeof(*dm));
+                k = wave_propagation(num_block, u, f, dm, N, h);
+                double t2 = omp_get_wtime();
+                double tend_ie = t2 - t1;
 
-            printf("N: %d. ", N);
-            printf("Thread: %d. ", threads);
-            printf("Time: %f sec. ", tend_ie);
-            printf("iteration: %d.\n", k);
+                printf("N: %d. ", N);
+                printf("Thread: %d. ", threads);
+                printf("Time: %f sec. ", tend_ie);
+                printf("iteration: %d.\n", k);
 
-            free_memory(u, f, dm, N);
+                free_memory(u, f, dm, N);
+            }
         }
+        printf("\n");
     }
 
     return 1;
